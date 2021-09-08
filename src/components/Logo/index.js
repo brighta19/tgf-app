971b2899f469bc9dd2985c92f9f1a63e13a90753
Bright Amoateng <bright.amoateng150@gmail.com>
2021-09-08 04:12:33 -0400

    Add icons and manifest file

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,19 +1,9 @@
 import React from "react";
-import TGF_SVG from "../../assets/tgf.svg";
 import "./style.css";
 
 class Logo extends React.Component {
   render() {
-    return (
-      <div className="Logo">
-        <div className="left">
-          <img src={TGF_SVG} alt="The Glass Files logo" />
-        </div>
-        <div className="right">
-          <span>The &nbsp;Glass Files</span>
-        </div>
-      </div>
-    );
+    return <div className="Logo"></div>;
   }
 }
 
